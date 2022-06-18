64a9ab135959b0bb994f75b0f5e46f6b7f8998e0
zeppipi <hazaelfch@gmail.com>
2022-06-18 22:40:54 +0800

    Game has been pushed to git!
    Now the game can be worked on both laptop and PC

---
+++ Assets/Scripts/PaddleFollow.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleFollow : MonoBehaviour
+{
+    private PositionConstraints constraintInstatiate;
+    
+    private GameObject playerPaddle;
+    private Transform paddleForm;
+
+    [SerializeField]
+    private Vector2 mousePos;
+    public float lerpSpeed = 1f;
+
+    [SerializeField]
+    private float x_size = 1f;
+    [SerializeField]
+    private float y_size = 1f;
+    
+    // Initialize befor anything starts
+    void Awake()
+    {
+        playerPaddle = this.gameObject;
+        paddleForm = playerPaddle.GetComponent<Transform>();
+
+        constraintInstatiate = this.GetComponent<PositionConstraints>();
+    }
+
+    // Initialize the first frame
+    void Start() 
+    {
+        playerPaddle.transform.localScale = new Vector3(x_size, y_size, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        playerPaddle.transform.position = Vector2.Lerp(playerPaddle.transform.position, mousePos, lerpSpeed);
+
+        if(mousePos.x > constraintInstatiate.constraintsGetter()[0] + constraintInstatiate.constraintsLocationGetter()[0] || mousePos.x < -constraintInstatiate.constraintsGetter()[0] + constraintInstatiate.constraintsLocationGetter()[0])
+        {
+            playerPaddle.transform.position = new Vector2(constraintInstatiate.constraintsGetter()[0] * (mousePos.x/Mathf.Abs(mousePos.x)) + constraintInstatiate.constraintsLocationGetter()[0], mousePos.y);
+        }
+        
+        if(mousePos.y > constraintInstatiate.constraintsGetter()[1] + constraintInstatiate.constraintsLocationGetter()[1] || mousePos.y < -constraintInstatiate.constraintsGetter()[1] + constraintInstatiate.constraintsLocationGetter()[1])
+        {
+            playerPaddle.transform.position = new Vector2(mousePos.x, constraintInstatiate.constraintsGetter()[1] * (mousePos.y/Mathf.Abs(mousePos.y)) + constraintInstatiate.constraintsLocationGetter()[1]);
+        }
+
+        //if(Mathf.Abs(mousePos.x) > constraintInstatiate.constraintsGetter()[0] + constraintInstatiate.constraintsLocationGetter()[0] && Mathf.Abs(mousePos.y) > constraintInstatiate.constraintsGetter()[1] + constraintInstatiate.constraintsLocationGetter()[1])
+        //{
+          //  playerPaddle.transform.position = new Vector2(constraintInstatiate.constraintsGetter()[0] * (mousePos.x/Mathf.Abs(mousePos.x)) + constraintInstatiate.constraintsLocationGetter()[0], constraintInstatiate.constraintsGetter()[1] * (mousePos.y/Mathf.Abs(mousePos.y)) + constraintInstatiate.constraintsLocationGetter()[1]);
+        //}
+    }
+
+    //For encapusilation and also easy debugging
+    /**
+    To get the size set for the gameobject, 0 index for the x and 1 index for the y
+    */
+    public List<float> sizeGetter()
+    {
+        List<float> res = new List<float>();
+        
+        res.Add(x_size);
+        res.Add(y_size);
+
+        return res;
+    }
+
+    /**
+    Made so the mousePos calculation doesn't need to be done twice
+    */
+    public Vector2 getMousePos()
+    {
+        return mousePos;
+    }
+
+    /**
+    Made so the different scripts are referencing the same gameObject
+    */
+    public GameObject gameObjectGetter()
+    {
+        return playerPaddle;
+    }
+}
---
+++ Assets/Scripts/PositionConstraints.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionConstraints : MonoBehaviour
+{
+    private PaddleFollow paddleInstantiate;
+    
+    private float paddle_x_size;
+    private float paddle_y_size;
+
+    //Set the maximum area
+    [SerializeField]
+    private float xConstraint = 1f;
+    [SerializeField]
+    private float yConstraint = 1f;
+
+    //Set the location area
+    [SerializeField]
+    private float xLocation = 0f;
+    [SerializeField]
+    private float yLocation = 0f;
+
+    private float xConstraint_res = 0f;
+    private float yConstraint_res = 0f;
+
+    //Instantiate before anything
+    void Awake()
+    {
+        paddleInstantiate = this.GetComponent<PaddleFollow>();
+        paddle_x_size = paddleInstantiate.sizeGetter()[0];
+        paddle_y_size = paddleInstantiate.sizeGetter()[1];
+    }
+    
+    //Render Gizmos
+    void OnDrawGizmos() 
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);
+        Gizmos.DrawCube(new Vector3(xLocation, yLocation, 1f), new Vector3(xConstraint, yConstraint, 1f));
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        xConstraint_res = xConstraint / 2 - paddle_x_size/2;
+        yConstraint_res = yConstraint / 2 - paddle_y_size/2;
+    }
+
+    /**
+    Result from this script's calculation has to be sent back to the main script since two scripts can't control the game gameObject
+    */
+    public List<float> constraintsGetter()
+    {
+        List<float> res = new List<float>();
+
+        res.Add(xConstraint_res);
+        res.Add(yConstraint_res);
+
+        return res;
+    }
+
+    public List<float> constraintsLocationGetter()
+    {
+        List<float> res = new List<float>();
+
+        res.Add(xLocation);
+        res.Add(yLocation);
+
+        return res;
+    }
+}
