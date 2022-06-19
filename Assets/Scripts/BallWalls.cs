32dc8ccd3d30c6a664f52ad70848ac27b1cbafa2
zeppipi <hazaelfch@gmail.com>
2022-06-19 21:07:46 +0800

    Some progress on the ball's walls made
    The script for the implementation has been partially made

--- Assets/Scripts/BallWalls.cs
+++ Assets/Scripts/BallWalls.cs
@@ -7,16 +7,27 @@ public class BallWalls : MonoBehaviour
     //Instantiate the entity manager script and object
     private GameObject informationObject;
     private Information informationScript;
+
+    //The box colliders for the edges of the screen
+    [SerializeField]
+    private BoxCollider2D leftWall;
+    [SerializeField]
+    private BoxCollider2D rightWall;
+
+    private PositionConstraints playArea;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        informationObject = GameObject.Find("EntitiesManager");
+        informationScript = informationObject.GetComponent<Information>();
+
+        playArea = informationScript.prefabListGetter(0).GetComponent<PositionConstraints>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        leftWall.offset = new Vector2(this.transform.position.x - playArea.constraintsGetter()[0], this.transform.position.y);
     }
 }
