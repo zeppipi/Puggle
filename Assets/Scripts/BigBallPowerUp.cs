a567598be4655b5bc3b0f785b6002d82797f318e
zeppipi <hazaelfch@gmail.com>
2022-06-26 17:24:54 +0800

    Big Ball Power Up has been implemented
    Power Up that makes the balls bigger has been made.
    
    A positive consequence is how this power up stacks which is pretty cool.
    
    A negative consequence is how if a new ball spawns while the power up is happening, the new spawned ball will not be big, but I'm keeping that because I think that causes more chaos which is cool.

---
+++ Assets/Scripts/BigBallPowerUp.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBallPowerUp : PowerUpBase
+{
+    //Get the balls
+    private GameObject[] balls;
+
+    //Set the parameters of the powerup
+    [SerializeField]
+    private float extraSize;
+    [SerializeField]
+    private float time;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+        balls = GameObject.FindGameObjectsWithTag("Ball");
+    }
+
+    public override void playPowerUp()
+    {
+        for(int index = 0; index < balls.Length; index++)
+        {
+            balls[index].transform.localScale = balls[index].transform.localScale + new Vector3(extraSize, extraSize, 0f);
+        }
+        StartCoroutine(CountDown(time));
+    }
+
+    IEnumerator CountDown(float time)
+    {
+        yield return new WaitForSeconds(time);
+        powerUpReturn();
+    }
+
+    private void powerUpReturn()
+    {
+        for(int index = 0; index < balls.Length; index++)
+        {
+            balls[index].transform.localScale = balls[index].transform.localScale - new Vector3(extraSize, extraSize, 0f);
+        }
+        Destroy(this.gameObject);
+    }
+    
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+        this.GetComponent<SpriteRenderer>().enabled = false;
+        this.GetComponent<CircleCollider2D>().enabled = false;
+        playPowerUp();
+    }
+}
