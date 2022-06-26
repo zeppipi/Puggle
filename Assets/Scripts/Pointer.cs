f5c20a64d2c299ca6169f84e2352a2fb03856b34
zeppipi <hazaelfch@gmail.com>
2022-06-26 18:00:12 +0800

    The beginnings of the pointer has been created
    The empty script for the pointer and the pointer gameobject has been created.
    
    Both does nothing for now for obvious reasons.

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    //Set height threshold
+    [SerializeField]
+    private float maxHeight;
+
+    //Insert the pointer gameobject here
+    [SerializeField]
+    private GameObject pointer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
