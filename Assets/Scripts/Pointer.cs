47398323a6e853651c6b0334523dfeb00040e8da
zeppipi <hazaelfch@gmail.com>
2022-06-27 17:33:32 +0800

    Pointer is now implemented
    The pointer system is fully implemented! Keep in mind the placeholder gameobject for it is just a square, which is quite poor at being a pointer, so just turn it into an actual pointer when the art comes.

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -12,9 +12,33 @@ public class Pointer : MonoBehaviour
     [SerializeField]
     private GameObject pointer;
 
+    //Make the pointer game object exist
+    void Start()
+    {
+        pointer = Instantiate(pointer, new Vector3(transform.position.x, maxHeight, 0f), Quaternion.identity);
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        //Make it follow the ball in the x axis continously
+        pointer.transform.position = new Vector2(transform.position.x, maxHeight);
+
+        //Gets smaller as the ball gets higher and forces it to become 0 if the ball ends up under the pointer
+        if(this.transform.position.y < maxHeight)
+        {
+            pointer.transform.localScale = new Vector2(0f, 0f);
+        }
+        else
+        {
+            pointer.transform.localScale = new Vector2(maxHeight/this.transform.position.y, maxHeight/this.transform.position.y);
+        }
+    }
+
+    //Gizmos for debugging purposes
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(new Vector2(0f, maxHeight), 0.3f);
     }
 }
