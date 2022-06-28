2454a07b65f7bfd3dc9f623d1ff08d59f4c49c3f
zeppipi <hazaelfch@gmail.com>
2022-06-28 23:48:48 +0800

    The special powerup spawner has been implemented
    The spawner for the special powerup, which is the split powerup, has been made
    
    Next step is to make the losing condition and reset
    
    Other concerns to worry about is to make more powerups and fix the occasional bug from 'ballwalls'

---
+++ Assets/Scripts/SpecialPowerUpSpawnner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialPowerUpSpawnner : MonoBehaviour
+{
+    /*
+    This script and gameobject is specifically made for the split power up, which is the power up that creates the increasing diff curve of the game
+    **/
+    
+    //Put the power up here
+    [SerializeField]
+    private GameObject specialPowerUp;
+
+    //Set how much score is needed to spawn
+    [SerializeField]
+    private int scoreNeeded;
+
+    //Get information about the score
+    private GameObject scoreManagerGameObject;
+    private Score scoreManagerScript;
+
+    //Temporary variable
+    private int lastScore;
+    private GameObject currentSpecialPowerUp;
+    [SerializeField]
+    private GameObject[] ballsScene;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        lastScore = 0;
+        scoreManagerGameObject = GameObject.Find("ScoreManager");
+        scoreManagerScript = scoreManagerGameObject.GetComponent<Score>();
+    }
+
+    //Update is called once per frame
+    void Update()
+    {   
+        ballsScene = GameObject.FindGameObjectsWithTag("Ball");
+        
+        if((scoreManagerScript.scoreGetter() - lastScore) >= scoreNeeded && scoreManagerScript.scoreGetter() > 0)
+        {
+            spawnSpecialPowerUp();
+            lastScore = scoreManagerScript.scoreGetter();
+        }
+    }
+
+    //Function to spawn the special powerup
+    private void spawnSpecialPowerUp()
+    {
+        if(currentSpecialPowerUp == null)
+        {
+            currentSpecialPowerUp = Instantiate(specialPowerUp, this.transform.position, Quaternion.identity);
+            scoreNeeded = scoreNeeded * ballsScene.Length;
+        }
+    }
+}
